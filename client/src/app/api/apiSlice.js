4654fae21a522c2097cf0169da0d5cda921445b1
MEZ901 <issammez44@gmail.com>
2023-12-17 14:31:12 +0100

    add top bar

---
+++ client/src/app/api/apiSlice.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:8080/api/",
+  credentials: "include",
+  prepareHeaders: (headers) => {
+    headers.set("content-type", "application/json");
+    headers.set("accept", "application/json");
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 403) {
+    console.log("Sending refresh token ...");
+
+    const refreshResult = await baseQuery(
+      "auth/refresh-token",
+      api,
+      extraOptions
+    );
+
+    if (refreshResult?.error) {
+      console.log("Refresh token failed");
+      //   api.dispatch(logOut());
+      localStorage.removeItem("user");
+    } else {
+      console.log("Refresh token success");
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
+
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryWithReauth,
+  // eslint-disable-next-line no-unused-vars
+  endpoints: (builder) => ({}),
+});
---
+++ client/src/hooks/useMode.js
@@ -0,0 +1,20 @@
+import { useMemo, useState } from "react";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "../theme";
+
+const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return [theme, colorMode];
+};
+
+export default useMode;
