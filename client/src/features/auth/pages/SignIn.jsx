85b9775490e9b8e1d5a69149e181f105dd162603
MEZ901 <issammez44@gmail.com>
2023-12-18 22:30:06 +0100

    collect data from sign up and sign in using formik

--- client/src/features/auth/pages/SignIn.jsx
+++ client/src/features/auth/pages/SignIn.jsx
@@ -1,6 +1,5 @@
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
@@ -8,13 +7,35 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const SignIn = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
+  const emailRule = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const loginSchema = yup.object().shape({
+    email: yup
+      .string()
+      .matches(emailRule, "Please enter a valid email")
+      .required("Email is required"),
+    password: yup.string().min(6).required("Password is required"),
+  });
+
+  const { values, errors, touched, handleChange, handleSubmit, handleBlur } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validationSchema: loginSchema,
+      onSubmit: async ({ email, password }) => {
+        console.log({ email, password });
+      },
+    });
+
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
       <Box
         sx={{
           marginTop: 8,
@@ -29,75 +50,77 @@ const SignIn = () => {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box
-          display="grid"
-          gap="30px"
-          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-          sx={{
-            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-            marginTop: 4,
-          }}
-        >
-          <TextField
-            fullWidth
-            variant="filled"
-            type="text"
-            label="Email"
-            // onBlur={handleBlur}
-            // onChange={handleChange}
-            // value={values.email}
-            name="email"
-            // error={!!touched.email && !!errors.email}
-            // helperText={touched.email && errors.email}
-            sx={{ gridColumn: "span 16" }}
-          />
-          <TextField
-            fullWidth
-            variant="filled"
-            type="password"
-            label="Password"
-            // onBlur={handleBlur}
-            // onChange={handleChange}
-            // value={values.address2}
-            name="password"
-            // error={!!touched.address2 && !!errors.address2}
-            // helperText={touched.address2 && errors.address2}
-            sx={{ gridColumn: "span 16" }}
-          />
-        </Box>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          mt="20px"
-          sx={{ width: "100%" }}
-        >
-          <Box>
-            <Link href="#" style={{ color: "white" }}>
-              Forgot password?
-            </Link>
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            sx={{
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              marginTop: 4,
+            }}
+          >
+            <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={!!touched.email && !!errors.email}
+              helperText={touched.email && errors.email}
+              sx={{ gridColumn: "span 16" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="password"
+              label="Password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={!!touched.password && !!errors.password}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: "span 16" }}
+            />
           </Box>
-          <Box>
-            <Link to="signup" style={{ color: "white" }}>
-              Don&apos;t have an account? Sign Up
-            </Link>
-          </Box>
-        </Box>
-        <Box
-          display="flex"
-          justifyContent="end"
-          mt="20px"
-          sx={{ width: "100%" }}
-        >
-          <Button
-            type="submit"
-            color="secondary"
-            variant="contained"
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mt="20px"
             sx={{ width: "100%" }}
           >
-            Sign in
-          </Button>
-        </Box>
+            <Box>
+              <Link href="#" style={{ color: "white" }}>
+                Forgot password?
+              </Link>
+            </Box>
+            <Box>
+              <Link to="signup" style={{ color: "white" }}>
+                Don&apos;t have an account? Sign Up
+              </Link>
+            </Box>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="end"
+            mt="20px"
+            sx={{ width: "100%" }}
+          >
+            <Button
+              type="submit"
+              color="secondary"
+              variant="contained"
+              sx={{ width: "100%" }}
+            >
+              Sign in
+            </Button>
+          </Box>
+        </form>
       </Box>
     </Container>
   );
--- client/src/features/auth/pages/SignUp.jsx
+++ client/src/features/auth/pages/SignUp.jsx
@@ -1,6 +1,5 @@
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
@@ -8,118 +7,148 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const SignUp = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
+  const emailRule = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const registerSchema = yup.object().shape({
+    username: yup.string().required("Username is required"),
+    email: yup
+      .string()
+      .matches(emailRule, "Please enter a valid email")
+      .required("Email is required"),
+    password: yup.string().min(6).required("Password is required"),
+    confirmPassword: yup
+      .string()
+      .required("Password confirmation is required")
+      .oneOf([yup.ref("password"), null], "Passwords must match"),
+  });
+  const { values, errors, touched, handleChange, handleSubmit, handleBlur } =
+    useFormik({
+      initialValues: {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+      validationSchema: registerSchema,
+      onSubmit: async (data) => {
+        console.log(data);
+      },
+    });
+
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
+      <form onSubmit={handleSubmit}>
         <Box
-          display="grid"
-          gap="30px"
-          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
           sx={{
-            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-            marginTop: 4,
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <TextField
-            fullWidth
-            variant="filled"
-            type="text"
-            label="Username"
-            // onBlur={handleBlur}
-            // onChange={handleChange}
-            // value={values.username}
-            name="username"
-            // error={!!touched.username && !!errors.username}
-            // helperText={touched.username && errors.username}
-            sx={{ gridColumn: "span 16" }}
-          />
-          <TextField
-            fullWidth
-            variant="filled"
-            type="text"
-            label="Email"
-            // onBlur={handleBlur}
-            // onChange={handleChange}
-            // value={values.email}
-            name="email"
-            // error={!!touched.email && !!errors.email}
-            // helperText={touched.email && errors.email}
-            sx={{ gridColumn: "span 16" }}
-          />
-          <TextField
-            fullWidth
-            variant="filled"
-            type="password"
-            label="Password"
-            // onBlur={handleBlur}
-            // onChange={handleChange}
-            // value={values.password}
-            name="password"
-            // error={!!touched.password && !!errors.password}
-            // helperText={touched.password && errors.password}
-            sx={{ gridColumn: "span 16" }}
-          />
-          <TextField
-            fullWidth
-            variant="filled"
-            type="password"
-            label="Confirm Password"
-            // onBlur={handleBlur}
-            // onChange={handleChange}
-            // value={values.confirmPassword}
-            name="confirmPassword"
-            // error={!!touched.confirmPassword && !!errors.confirmPassword}
-            // helperText={touched.confirmPassword && errors.confirmPassword}
-            sx={{ gridColumn: "span 16" }}
-          />
-        </Box>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          mt="20px"
-          sx={{ width: "100%" }}
-        >
-          <Box>
-            <Link to="/" style={{ color: "white" }}>
-              Already have an account? Sign in
-            </Link>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            sx={{
+              "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              marginTop: 4,
+            }}
+          >
+            <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Username"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.username}
+              name="username"
+              error={!!touched.username && !!errors.username}
+              helperText={touched.username && errors.username}
+              sx={{ gridColumn: "span 16" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="text"
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={!!touched.email && !!errors.email}
+              helperText={touched.email && errors.email}
+              sx={{ gridColumn: "span 16" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="password"
+              label="Password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={!!touched.password && !!errors.password}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: "span 16" }}
+            />
+            <TextField
+              fullWidth
+              variant="filled"
+              type="password"
+              label="Confirm Password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.confirmPassword}
+              name="confirmPassword"
+              error={!!touched.confirmPassword && !!errors.confirmPassword}
+              helperText={touched.confirmPassword && errors.confirmPassword}
+              sx={{ gridColumn: "span 16" }}
+            />
           </Box>
-        </Box>
-        <Box
-          display="flex"
-          justifyContent="end"
-          mt="20px"
-          sx={{ width: "100%" }}
-        >
-          <Button
-            type="submit"
-            color="secondary"
-            variant="contained"
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mt="20px"
             sx={{ width: "100%" }}
           >
-            Sign up
-          </Button>
+            <Box>
+              <Link to="/" style={{ color: "white" }}>
+                Already have an account? Sign in
+              </Link>
+            </Box>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="end"
+            mt="20px"
+            sx={{ width: "100%" }}
+          >
+            <Button
+              type="submit"
+              color="secondary"
+              variant="contained"
+              sx={{ width: "100%" }}
+            >
+              Sign up
+            </Button>
+          </Box>
         </Box>
-      </Box>
+      </form>
     </Container>
   );
 };
